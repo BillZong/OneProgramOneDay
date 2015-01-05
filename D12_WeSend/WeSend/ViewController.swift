bb0038c2b913a01e7cf8c0b8d436e36e957e785f
Bill Zong <billzong2012@gmail.com>
2015-01-05 21:03:38 +0800

    Copying the Weixin, the course is from www.xiaoboswift.com

---
+++ D12_WeSend/WeSend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeSend
+//
+//  Created by BillZong on 15-1-4.
+//  Copyright (c) 2015年 BillZong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ D12_WeSend/WeSend/WSMessage.swift
@@ -0,0 +1,13 @@
+//
+//  WSMessage.swift
+//  WeSend
+//
+//  Created by BillZong on 15-1-4.
+//  Copyright (c) 2015年 BillZong. All rights reserved.
+//
+
+import Foundation
+
+struct WSMessage {
+    
+}
